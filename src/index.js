70500b4b4e66b0f4f88d3f697128ab5b1b4e9638
Sokrady Chey <sokradychey@gmail.com>
2022-11-01 13:12:35 -0400

    Organizing directories

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Navigation from './Navigation';
+import Navigation from './Navigation/Navigation';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
